070ac72e6b585f1191e9175d89f109ec2a6f7ad3
fares-ubidata <131752202+fares-ubidata@users.noreply.github.com>
2023-08-08 12:54:02 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ APK_FILE="build/app/outputs/flutter-apk/app-release.apk"
 APP_VERSION="1.0.2"
 
 # Création de l'URL de déploiement
-DEPLOY_URL="${NEXUS_URL}com/example/flutter-apk-artifact/${APP_VERSION}/flutter-apk-artifact-${APP_VERSION}.apk"
+DEPLOY_URL="${NEXUS_URL}/projects/${APP_VERSION}/flutter-apk-${APP_VERSION}.apk"
 
 # Utilisation de curl pour le déploiement
 curl -v -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" --upload-file "${APK_FILE}" "${DEPLOY_URL}"
