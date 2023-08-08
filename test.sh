f9e431962ea9a034982affc1d967bd135b07f45b
fares-ubidata <131752202+fares-ubidata@users.noreply.github.com>
2023-08-08 12:56:53 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+FLUTTER_APP_NAME=$(grep "name:" pubspec.yaml | awk -F ": " '{ print $2 }')
+
+# Paramètres de l'application
+APP_NAME="${FLUTTER_APP_NAME}"
 
 # Paramètres du référentiel Nexus
 NEXUS_URL="https://fcfa-62-72-97-221.ngrok-free.app/repository/flutter-apk/"
@@ -11,7 +15,7 @@ APK_FILE="build/app/outputs/flutter-apk/app-release.apk"
 APP_VERSION="1.0.2"
 
 # Création de l'URL de déploiement
-DEPLOY_URL="${NEXUS_URL}/projects/${APP_VERSION}/flutter-apk-${APP_VERSION}.apk"
+DEPLOY_URL="${NEXUS_URL}/${APP_NAME}/${APP_VERSION}/flutter-apk-${APP_VERSION}.apk"
 
 # Utilisation de curl pour le déploiement
 curl -v -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" --upload-file "${APK_FILE}" "${DEPLOY_URL}"
